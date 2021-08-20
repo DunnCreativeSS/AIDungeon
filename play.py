9f28f05a3ffb751f4d1ca9ed9a0594a3d06c387a
Jarett Dunn <jd@coindexcap.com>
2021-08-20 04:13:23 -0300

    changes

--- play.py
+++ play.py
@@ -4,6 +4,7 @@ import random
 import sys
 import time
 import argparse
+import flask
 
 from generator.gpt2.gpt2_generator import *
 from story import grammars
@@ -18,6 +19,93 @@ parser.add_argument(
     action="store_true",
     help="Force using CPU instead of GPU."
 )
+
+from flask import request, redirect
+generator = GPT2Generator(force_cpu=True)
+from flask import Flask
+ 
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/lit", methods=["GET", "POST"])
+def lit():
+    global generator, story_manager
+
+    req = request.form
+    q = req['question_raw']
+    uuid = req['uuid']
+    d = ""
+    if uuid not in story_manager:
+        story_manager[uuid] = UnconstrainedStoryManager(generator)
+        print("\n")
+
+    if story_manager[uuid].story != None:
+        story_manager[uuid].story = None
+
+    while story_manager[uuid].story is None:
+            print("\n\n")
+    
+            #context, prompt = get_custom_prompt()
+            context = ""
+            
+            prompt = q
+
+            result = story_manager[uuid].start_new_story(
+            prompt, context=context, upload_story=False 
+        )
+
+
+    sys.stdin.flush()
+    action = (("> ") + q).strip()
+
+    if action == "":
+        action = ""
+        result = story_manager[uuid].act(action)
+        d = d + (result)
+
+    elif action[0] == '"':
+        action = "You say " + action
+
+    else:
+        action = action.strip()
+
+        if "you" not in action[:6].lower() and "I" not in action[:6]:
+            action = action[0].lower() + action[1:]
+            action = "You " + action
+
+        if action[-1] not in [".", "?", "!"]:
+            action = action + "."
+
+        action = first_to_second_person(action)
+
+        action = "\n> " + action + "\n"
+
+    result = "\n" + story_manager[uuid].act(action)
+    if len(story_manager[uuid].story.results) >= 2:
+        similarity = get_similarity(
+            story_manager[uuid].story.results[-1], story_manager[uuid].story.results[-2]
+        )
+        if similarity > 0.9:
+            story_manager[uuid].story.actions = story_manager[uuid].story.actions[:-1]
+            story_manager[uuid].story.results = story_manager[uuid].story.results[:-1]
+            d = d + (
+                "Woops that action caused the model to start looping. Try a different action to prevent that."
+            )
+            #continue
+
+    if player_won(result):
+        d = d + (result + "\n CONGRATS YOU WIN")
+        story_manager[uuid].story.get_rating()
+        #break
+    elif player_died(result):
+        d = d + (result)
+        
+
+    else:
+        d = d + (result)
+    #return redirect(request.url)
+
+    return d
 
 
 def splash():
@@ -153,7 +241,7 @@ def instructions():
     text += '\n  "/censor off/on" to turn censoring off or on.'
     return text
 
-
+story_manager = {}
 def play_aidungeon_2(args):
     """
     Entry/main function for starting AIDungeon 2
@@ -172,212 +260,10 @@ def play_aidungeon_2(args):
     upload_story = True
 
     print("\nInitializing AI Dungeon! (This might take a few minutes)\n")
-    generator = GPT2Generator(force_cpu=args.cpu)
-    story_manager = UnconstrainedStoryManager(generator)
-    print("\n")
+    
 
-    with open("opening.txt", "r", encoding="utf-8") as file:
-        starter = file.read()
-    print(starter)
-
-    while True:
-        if story_manager.story != None:
-            story_manager.story = None
-
-        while story_manager.story is None:
-            print("\n\n")
-            splash_choice = splash()
-
-            if splash_choice == "new":
-                print("\n\n")
-                (
-                    setting_key,
-                    character_key,
-                    name,
-                    character,
-                    setting_description,
-                ) = select_game()
-
-                if setting_key == "custom":
-                    context, prompt = get_custom_prompt()
-
-                else:
-                    context, prompt = get_curated_exposition(
-                        setting_key, character_key, name, character, setting_description
-                    )
-
-                console_print(instructions())
-                print("\nGenerating story...")
-
-                result = story_manager.start_new_story(
-                    prompt, context=context, upload_story=upload_story
-                )
-                print("\n")
-                console_print(result)
-
-            else:
-                load_ID = input("What is the ID of the saved game? ")
-                result = story_manager.load_new_story(
-                    load_ID, upload_story=upload_story
-                )
-                print("\nLoading Game...\n")
-                console_print(result)
-
-        while True:
-            sys.stdin.flush()
-            action = input("> ").strip()
-            if len(action) > 0 and action[0] == "/":
-                split = action[1:].split(" ")  # removes preceding slash
-                command = split[0].lower()
-                args = split[1:]
-                if command == "reset":
-                    story_manager.story.get_rating()
-                    break
-
-                elif command == "restart":
-                    story_manager.story.actions = []
-                    story_manager.story.results = []
-                    console_print("Game restarted.")
-                    console_print(story_manager.story.story_start)
-                    continue
-
-                elif command == "quit":
-                    story_manager.story.get_rating()
-                    exit()
-
-                elif command == "nosaving":
-                    upload_story = False
-                    story_manager.story.upload_story = False
-                    console_print("Saving turned off.")
-
-                elif command == "help":
-                    console_print(instructions())
-
-                elif command == "censor":
-                    if len(args) == 0:
-                        if generator.censor:
-                            console_print("Censor is enabled.")
-                        else:
-                            console_print("Censor is disabled.")
-                    elif args[0] == "off":
-                        if not generator.censor:
-                            console_print("Censor is already disabled.")
-                        else:
-                            generator.censor = False
-                            console_print("Censor is now disabled.")
-
-                    elif args[0] == "on":
-                        if generator.censor:
-                            console_print("Censor is already enabled.")
-                        else:
-                            generator.censor = True
-                            console_print("Censor is now enabled.")
-
-                    else:
-                        console_print("Invalid argument: {}".format(args[0]))
-
-                elif command == "save":
-                    if upload_story:
-                        id = story_manager.story.save_to_storage()
-                        console_print("Game saved.")
-                        console_print(
-                            "To load the game, type 'load' and enter the "
-                            "following ID: {}".format(id)
-                        )
-                    else:
-                        console_print("Saving has been turned off. Cannot save.")
-
-                elif command == "load":
-                    if len(args) == 0:
-                        load_ID = input("What is the ID of the saved game?")
-                    else:
-                        load_ID = args[0]
-                    result = story_manager.story.load_from_storage(load_ID)
-                    console_print("\nLoading Game...\n")
-                    console_print(result)
-
-                elif command == "print":
-                    print("\nPRINTING\n")
-                    print(str(story_manager.story))
-
-                elif command == "revert":
-                    if len(story_manager.story.actions) == 0:
-                        console_print("You can't go back any farther. ")
-                        continue
-
-                    story_manager.story.actions = story_manager.story.actions[:-1]
-                    story_manager.story.results = story_manager.story.results[:-1]
-                    console_print("Last action reverted. ")
-                    if len(story_manager.story.results) > 0:
-                        console_print(story_manager.story.results[-1])
-                    else:
-                        console_print(story_manager.story.story_start)
-                    continue
-
-                else:
-                    console_print("Unknown command: {}".format(command))
-
-            else:
-                if action == "":
-                    action = ""
-                    result = story_manager.act(action)
-                    console_print(result)
-
-                elif action[0] == '"':
-                    action = "You say " + action
-
-                else:
-                    action = action.strip()
-
-                    if "you" not in action[:6].lower() and "I" not in action[:6]:
-                        action = action[0].lower() + action[1:]
-                        action = "You " + action
-
-                    if action[-1] not in [".", "?", "!"]:
-                        action = action + "."
-
-                    action = first_to_second_person(action)
-
-                    action = "\n> " + action + "\n"
-
-                result = "\n" + story_manager.act(action)
-                if len(story_manager.story.results) >= 2:
-                    similarity = get_similarity(
-                        story_manager.story.results[-1], story_manager.story.results[-2]
-                    )
-                    if similarity > 0.9:
-                        story_manager.story.actions = story_manager.story.actions[:-1]
-                        story_manager.story.results = story_manager.story.results[:-1]
-                        console_print(
-                            "Woops that action caused the model to start looping. Try a different action to prevent that."
-                        )
-                        continue
-
-                if player_won(result):
-                    console_print(result + "\n CONGRATS YOU WIN")
-                    story_manager.story.get_rating()
-                    break
-                elif player_died(result):
-                    console_print(result)
-                    console_print("YOU DIED. GAME OVER")
-                    console_print("\nOptions:")
-                    console_print("0) Start a new game")
-                    console_print(
-                        "1) \"I'm not dead yet!\" (If you didn't actually die) "
-                    )
-                    console_print("Which do you choose? ")
-                    choice = get_num_options(2)
-                    if choice == 0:
-                        story_manager.story.get_rating()
-                        break
-                    else:
-                        console_print("Sorry about that...where were we?")
-                        console_print(result)
-
-                else:
-                    console_print(result)
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    play_aidungeon_2(args)
+    app.run(host='0.0.0.0', port=8086)
+
